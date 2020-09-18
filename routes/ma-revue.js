e754c95d1968678cd6e8e399f60bddab9acd6f9b
Moni <monica_be@hotmail.fr>
2020-09-18 23:05:03 +0200

    selection data query for books and revue

--- routes/ma-revue.js
+++ routes/ma-revue.js
@@ -4,7 +4,7 @@ const connection = require('../conf')
 const router = express.Router()
 
 router.get('/:id', (req, res) => {
-  connection.query('SELECT revue.id, title, number, price, description, image_name, name FROM revue JOIN category_revue ON category_revue.id=revue.id  WHERE revue.id = ?', [req.params.id], (err, results) => {
+  connection.query('SELECT id, title, number, price, image_name, description FROM revue WHERE revue.id = ?', [req.params.id], (err, results) => {
     if (err) {
       res.status(500).send('Erreur lors de la connection')
     } else if (results.length === 0) {
--- routes/mon-livre.js
+++ routes/mon-livre.js
@@ -4,7 +4,7 @@ const connection = require('../conf')
 const router = express.Router()
 
 router.get('/:id', (req, res) => {
-  connection.query('SELECT books.id, title, author,price, description, image_name,name FROM books JOIN category category ON category.id=books.id  WHERE books.id= ?', [req.params.id], (err, results) => {
+  connection.query('SELECT books.id, title, author,price, description, image_name,name FROM books JOIN category ON category.id=books.id  WHERE books.id= ?', [req.params.id], (err, results) => {
     if (err) {
       console.log(err)
       res.status(500).send('Erreur de connection')
