0779b9458cdf9a1d8d261b5cc0347d3953fca69c
Moni <monica_be@hotmail.fr>
2020-09-18 21:29:48 +0200

    setting revue.id to query

--- routes/ma-revue.js
+++ routes/ma-revue.js
@@ -4,13 +4,13 @@ const connection = require('../conf')
 const router = express.Router()
 
 router.get('/:id', (req, res) => {
-  connection.query('SELECT id, category_id, title, number,price, description, image_name FROM revue WHERE id = ?', [req.params.id], (err, results) => {
+  connection.query('SELECT revue.id, title, number, price, description, image_name, name FROM revue JOIN category_revue ON category_revue.id=revue.id  WHERE revue.id = ?', [req.params.id], (err, results) => {
     if (err) {
-      res.status(500).send('Erreur losr de la connection')
+      res.status(500).send('Erreur lors de la connection')
     } else if (results.length === 0) {
       res.status(404).send('C\'est produit n\'est plus disponible')
     } else {
-      res.json(results)
+      res.json(results[0])
     }
   })
 })
