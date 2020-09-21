859de455f800bb1841305b0c9188c66c2ea9a153
Moni <monica_be@hotmail.fr>
2020-09-21 20:00:16 +0200

    setting query for books page

--- routes/livre.js
+++ routes/livre.js
@@ -4,9 +4,9 @@ const connection = require('../conf')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  connection.query('SELECT books.id, category_id,title, author,price, description, image_name FROM books', (err, results) => {
+  connection.query('SELECT id,title, author,price, description, image_name, category_id FROM books', (err, results) => {
     if (err) {
-      res.status(500).send('Erreur losr de la connection')
+      res.status(500).send('Erreur lors de la connection')
     } else {
       res.json(results)
     }
--- routes/mon-livre.js
+++ routes/mon-livre.js
@@ -4,9 +4,9 @@ const connection = require('../conf')
 const router = express.Router()
 
 router.get('/:id', (req, res) => {
-  connection.query('SELECT books.id, title, author,price, description, image_name,name FROM books JOIN category ON category.id=books.id  WHERE books.id= ?', [req.params.id], (err, results) => {
+  connection.query('SELECT title, author,price, description, image_name FROM books  WHERE books.id= ?', [req.params.id], (err, results) => {
     if (err) {
-      console.log(err)
+      /* console.log(err) */
       res.status(500).send('Erreur de connection')
     } else if (results.length === 0) {
       res.status(404).send('C\'est produit n\'est plus disponible')
